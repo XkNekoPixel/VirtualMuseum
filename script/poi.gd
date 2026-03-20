8ced96655342a171114f1f15fd14413b3b3e7601
SrSteveYT <140319902+XkNekoPixel@users.noreply.github.com>
2026-03-20 20:06:45 +0500

    -

--- script/poi.gd
+++
@@ -1,8 +0,0 @@
-extends Area3D
-
-@export var title : String
-@export_multiline var description : String
-@export var image : Texture2D
-
-func show_info(panel):
-	panel.show_info(title, description, image)
