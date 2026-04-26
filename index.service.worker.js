e144ebbd8e610a7bd89b9540b61ef343e5db3fc4
SrSteveYT <140319902+XkNekoPixel@users.noreply.github.com>
2026-04-26 13:59:27 +0500

    update ui#2 Test

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1777191956|1495833302';
+const CACHE_VERSION = '1777193944|3483708024';
 /** @type {string} */
 const CACHE_PREFIX = 'VirtualMuseum-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
